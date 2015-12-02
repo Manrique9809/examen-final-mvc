31dbe235118f4f1a89fb3199a00e97af80d370b6
Antony Manrique <antony476@gmail.com>
2015-12-02 11:06:01 -0500

    reconfigurando config

--- application/config/config.php
+++ application/config/config.php
@@ -1,14 +1,14 @@
 <?php 
 
-$config['base_url'] = 'http://examen-final.herokuapp.com/mvc/'; 
+$config['base_url'] = 'http://examen-final.herokuapp.com/'; 
 
 $config['default_controller'] = 'games';  
 $config['error_controller'] = 'error';  
 
-$config['db_host'] = 'localhost';  
-$config['db_name'] = 'examen-final';  
-$config['db_username'] = 'root';  
-$config['db_password'] = 'root';  
+$config['db_host'] = 'tesismetro.comli.com';  
+$config['db_name'] = 'a7927207_examen';  
+$config['db_username'] = 'a7927207_examen';  
+$config['db_password'] = 'examen123';  
 
 
 
